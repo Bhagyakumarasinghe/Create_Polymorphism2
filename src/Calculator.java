a721251a93c6d4f183216f5fcb1c8f07a9c7a1b8
Nipuni Bhagya <nipunibagya20010511@gmail.com>
2023-10-04 11:16:44 +0530

    first commit

---
+++ src/Calculator.java
@@ -0,0 +1,30 @@
+public class Calculator {
+        public int add(int a, int b) {
+            return a + b;
+        }
+
+
+        public int add(int a, int b, int c) {
+            return a + b + c;
+        }
+
+
+        public double add(double a, double b) {
+            return a + b;
+        }
+    }
+
+
+     class Main {
+        public static void main(String[] args) {
+            Calculator calculator = new Calculator();
+            int sum1 = calculator.add(5, 10);
+            int sum2 = calculator.add(5, 10, 15);
+            double sum3 = calculator.add(2.5, 3.7);
+            System.out.println("Sum 1: " + sum1);
+            System.out.println("Sum 2: " + sum2);
+            System.out.println("Sum 3: " + sum3);
+        }
+    }
+
+
